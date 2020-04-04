6acc43a243fdd68bc788cfe64c66c3676afd510c
Martin Markov <I425706@fhict.nl>
2020-04-04 20:09:45 +0200

    Initialized laravel project and added users, administrators, workers, managers and departments models

---
+++ WebApp/app/Worker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Worker extends Model
+{
+    protected $fillable = [
+        'user_id', 'department_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo('App\Department');
+    }
+}
