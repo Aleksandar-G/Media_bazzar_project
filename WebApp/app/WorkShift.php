73903dd277602daefea03dfb3d5efbf3dba968d5
Martin Markov <I425706@fhict.nl>
2020-05-31 12:44:06 +0300

    Added workshifts migrations for creating the workers_work_shifts table and adding workers_needed and wage to the work_shifts table and model

--- WebApp/app/WorkShift.php
+++ WebApp/app/WorkShift.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class WorkShift extends Model
 {
     protected $fillable = [
-        'shift', 'date', 'worker_id'
-     ];
+        'shift', 'date', 'wage', 'workers_needed'
+    ];
 
-    public function worker()
+    public function workers()
     {
-        return $this->belongsTo('App\Worker');
+        return $this->belongsToMany('App\Worker', 'workers_work_shifts')->withTimestamps();
     }
+
+    # TODO: Validate the number of workers
+    // protected static function validateWorkers($model) {
+    //     if ($model->workers_needed < $model->workers->count()) {
+    //         throw new Exception("Shift is full");
+    //     }
+    // }
 }
